be07be0e27c554506f3c22f79f96298a47c343fd
Uthman <limitz838@gmail.com>
2026-04-19 09:31:21 -0700

    connect routers

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
+from app.routers import admin, client, signals
+
 app = FastAPI(title="NolimitzBots Backend", version="1.0.0")
 
 
 @app.get("/")
 def root():
     return {"message": "NolimitzBots backend is live"}
+
+
+# CONNECT ROUTERS
+app.include_router(admin.router)
+app.include_router(client.router)
+app.include_router(signals.router)
